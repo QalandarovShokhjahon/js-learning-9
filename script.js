ac6906d60b32cd675cd9c4ac0a8381b461e842bc
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2025-11-24 18:03:11 +0500

    initial

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Method → obyektning belgilangan vazifalarini bajarish uchun mo’ljallangan funksiyadir.
+
+//  Number methodlar
+
+// Math.round() → sonni eng yaqin butun songa yaxlitlaydi, agar o‘nlik qism 0.5 yoki undan katta bo‘lsa, raqam yuqoriga, aks holda pastga yaxlitlanadi.
